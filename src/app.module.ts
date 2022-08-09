1809b984dc2111b1998f7e2e7703fb65e0c0d9be
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-08-09 16:46:43 +0300

    add dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const HOST:string = process.env.MONGODB_HOST || 'localhost';
+const PORT:string = process.env.MONGODB_PORT || '27017';
+const DATABASE:string = process.env.MONGODB_DATABASE || 'test';
+const URI:string = `mongodb://${HOST}:${PORT}/${DATABASE}`;
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

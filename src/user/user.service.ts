934e22029e3d77779f1a7aa0a87574bf923d3211
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-08-09 17:17:39 +0300

    implement user registration

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/types/user';
+import { RegisterDTO } from './register.dto';
 
 @Injectable()
 export class UserService {
   constructor(
-    @InjectModel('User') private userModel: Model<User>
+    @InjectModel('User') private userModel: Model<User>,
   ) {}
+
+  async create(RegisterDTO: RegisterDTO) {
+    const { email } = RegisterDTO;
+    const user = await this.userModel.findOne({ email });
+    if (user) {
+        throw new HttpException('User already exist', HttpStatus.BAD_REQUEST);
+    }
+    const createdUser = new this.userModel(RegisterDTO);
+    await createdUser.save();
+    return this.sanitizeUser(createdUser);
+  }
+
+  sanitizeUser(user: User) {
+    const sanitized = user.toObject();
+    delete sanitized['password'];
+    return sanitized;
+  }
 }

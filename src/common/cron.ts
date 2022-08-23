50aef93985a7e21a5fa496173e5f66560c65dab3
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-08-23 11:28:50 +0300

    add restore DB function

--- src/common/cron.ts
+++ src/common/cron.ts
@@ -4,7 +4,7 @@ import { exec } from 'child_process';
 
 export default cron.schedule('*/5 * * * *', () => {
   exec(
-    "docker exec mymongodb sh -c 'mongodump --archive' study-project/db.dump",
+    "docker exec mymongodb sh -c 'mongodump --archive' > db.dump",
     (error, stdout, stderr) => {
       if (error) {
         console.log(`error: ${error.message}`);
@@ -17,3 +17,19 @@ export default cron.schedule('*/5 * * * *', () => {
     },
   );
 });
+
+export function restoreDB(): void {
+  exec(
+    "docker exec -i mymongodb sh -c 'mongorestore --archive < db.dump",
+    (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    },
+  );
+}
